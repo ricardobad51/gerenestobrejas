d81ca1e9e561d9bcaf5c5731699fe19295067ce1
ricardobad51 <rbbelem@gmail.com>
2022-01-04 01:20:56 -0300

    Created Project gerenestobrejas

---
+++ src/main/java/br/com/belemburitiricardo/gerenestobrejas/GerenestobrejasApplication.java
@@ -0,0 +1,13 @@
+package br.com.belemburitiricardo.gerenestobrejas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenestobrejasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenestobrejasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/belemburitiricardo/gerenestobrejas/GerenestobrejasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.belemburitiricardo.gerenestobrejas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenestobrejasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
